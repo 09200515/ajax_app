6b904f31f9bdb1bab2c873e3182c8233b3c8f698
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-02-12 12:22:24 +0900

    Basic認証を導入

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -19,12 +19,13 @@ function check() {
         }
         const item = XHR.response.post;
         if (item.checked === true) {
-          post.setAttribute("data-check", "true");
+          post.setAttribute("data-check", true);
         } else if (item.checked === false) {
           post.removeAttribute("data-check");
         }
       };
     });
-  });
+  })
 }
+
 setInterval(check, 1000);
